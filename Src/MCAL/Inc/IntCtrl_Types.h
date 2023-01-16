d064e09ccf886d1ee39ba3f2dcf4fee4899d10f5
Ahmed M. Rizk <Amadoxis@gmail.com>
2023-01-16 05:16:51 +0200

    NVIC Driver Version 1.0

--- Src/MCAL/Inc/IntCtrl_Types.h
+++ Src/MCAL/Inc/IntCtrl_Types.h
@@ -8,6 +8,10 @@
 /***************************************************************************************
  *                              Global Constant Macros                                 *
 ****************************************************************************************/
+#define XXX 0x0U
+#define XXY 0x5U
+#define XYY 0x6U
+#define YYY 0x7U
 
 /***************************************************************************************
  *                                Global Function Macros                               *
@@ -16,8 +20,32 @@
 /***************************************************************************************
  *                          Global Data Types and Structures                           *
 ****************************************************************************************/
+/*************Interrupt Vectors Enum*************/
+typedef enum{
+Reset_IRQ=1,
+NMI_IRQ,
+Hard_Fault_IRQ,
+Memory_Management_IRQ,
+Bus_Fault_IRQ,
+Usage_Fault_IRQ,
+SVCall_IRQ=11,
+Debug_Monitor_IRQ,
+PendSV_IRQ=14,
+SysTick_IRQ,
+GPIOA_IRQ,
+GPIOB_IRQ,
+GPIOC_IRQ,
+GPIOD_IRQ,
+GPIOE_IRQ,
+UART0_IRQ,
+UART1_IRQ
+}IntVectors_Type;
 
-
-
+/*************Interrupt Configs*************/
+typedef struct{
+        uint8 Enabled           [155];
+        uint8 Group_Priority    [155];
+        uint8 Sub_Group_Priority[155];
+}User_Config_IRQ_Type;
 
 #endif /*INTCTRL_TYPES_H_ */
--- Src/MCAL/IntCtrl.c
+++ Src/MCAL/IntCtrl.c
@@ -8,54 +8,147 @@
  * Details: ~Enables Interrupt Gates for Peripherals                                   *
  *           ~Configures Interrupt Priorities                                          *
  *                                                                                     *
-****************************************************************************************/
-
-
+ ****************************************************************************************/
 
 /***************************************************************************************
  *                                       Includes                                      *
-****************************************************************************************/
+ ****************************************************************************************/
 #include "IntCtrl.h"
-
 /***************************************************************************************
  *                             Local Macros Constant\Function                          *
-****************************************************************************************/
+ ****************************************************************************************/
 
 /***************************************************************************************
  *                                       Local Data                                    *
-****************************************************************************************/
-
+ ****************************************************************************************/
 
 /***************************************************************************************
  *                                       Global Data                                   *
-****************************************************************************************/
-
+ ****************************************************************************************/
 
 /***************************************************************************************
  *                                Local Function Prototypes                            *
-****************************************************************************************/
-
+ ****************************************************************************************/
+void IntCtrl_ConfigPriority(IntVectors_Type E_IntNum, uint8 u8_InteruptPriority);
+void IntCtrl_EnableIRQ(IntVectors_Type E_IntNum);
+void IntCtrl_DisableIRQ(IntVectors_Type E_IntNum);
 /***************************************************************************************
  *                                       Local Functions                               *
-****************************************************************************************/
+ ****************************************************************************************/
+void IntCtrl_ConfigPriority(IntVectors_Type E_IntNum, uint8 u8_InteruptPriority){
+    if(E_IntNum>SysTick_IRQ){
+        uint8 u8_InterruptNum=E_IntNum;
+        u8_InterruptNum-=16;
+        if(u8_InterruptNum%4==0){
+            PRI(u8_InterruptNum)|=((uint32)u8_InteruptPriority<<5);
+        }
+        else if(u8_InterruptNum%4==1){
+            PRI(u8_InterruptNum)|=((uint32)u8_InteruptPriority<<13);
+        }
+        else if(u8_InterruptNum%4==2){
+            PRI(u8_InterruptNum)|=((uint32)u8_InteruptPriority<<21);            
+        }
+        else{
+            PRI(u8_InterruptNum)|=((uint32)u8_InteruptPriority<<29);
+        }
 
+    }
+    else{
+        switch (E_IntNum)
+        {
+        case Memory_Management_IRQ:
+            SYSPRI->MEM = u8_InteruptPriority;
+            break;
+        case Bus_Fault_IRQ:
+            SYSPRI->BUS = u8_InteruptPriority;
+            break;
+        case Usage_Fault_IRQ:
+            SYSPRI->USAGE |= u8_InteruptPriority;
+            break;
+        case SVCall_IRQ:
+            SYSPRI->SVC = u8_InteruptPriority;
+            break;
+        case Debug_Monitor_IRQ:
+            SYSPRI->DEBUG = u8_InteruptPriority;
+            break;
+        case PendSV_IRQ:
+            SYSPRI->PENDSV = u8_InteruptPriority;
+            break;
+        case SysTick_IRQ:
+            SYSPRI->TICK = u8_InteruptPriority;
+            break;
+        default:
+            break;
+        }
+    }
+}
+void IntCtrl_EnableIRQ(IntVectors_Type E_IntNum){
+    if(E_IntNum>SysTick_IRQ){
+        uint8 u8_InterruptNum=E_IntNum;
+        SET_BIT(EN(u8_InterruptNum-16), u8_InterruptNum-32UL);
+    }
+    else{
+        switch (E_IntNum)
+        {
+        case Memory_Management_IRQ:
+            SYSHNDCTRL->B.MEM=1;
+            break;
+        case Bus_Fault_IRQ:
+            SYSHNDCTRL->B.BUS=1;
+            break;
+        case Usage_Fault_IRQ:
+            SYSHNDCTRL->B.USAGE=1;
+            break;
+        case SVCall_IRQ:
+            break;
+        case Debug_Monitor_IRQ:
+            break;
+        case PendSV_IRQ:
+            break;
+        case SysTick_IRQ:
+            break;
+        default:
+            break;
+        }
+    }
+}
+void IntCtrl_DisableIRQ(IntVectors_Type E_IntNum){
+}
 /***************************************************************************************
  *                                    API Implementations                              *
-****************************************************************************************/
-
-/***************************************************************************************
- *                                Local Function Prototypes                            *
-****************************************************************************************/
-
+ ****************************************************************************************/
 /***********************************************************
  * Syntax:
  * Description:
- * 
- * 
- * 
-*************************************************************/
-
+ *
+ *
+ *
+ *************************************************************/
+void IntCtrl_Init(User_Config_IRQ_Type *S_UserConfig){
+    uint8 u8_i;
+    uint8 u8_IRQ_Priority = 0, u8_Group_Priority, u8_Sub_Group_Priority;
+    /*Configure Grouping/Subgrouping Systems in APPINT register in SCB*/
+    APINT = (0x05FA << 16) | (Int_Priority << 8);
+    for (u8_i=1; u8_i <= 154; u8_i++)
+    {
+        /*Assign Grouping/Subgrouping priority in NVIC PRIx NVIC and SCB_SYSPRIx Registers*/
+#if Int_Priority == XXX
+        u8_IRQ_Priority = ((S_UserConfig->Group_Priority[u8_i]) & 0x7);
+#elif Int_Priority == XXY
+        u8_IRQ_Priority = (((S_UserConfig->Group_Priority[u8_i]) & 0x03) << 1UL) | ((S_UserConfig->Sub_Group_Priority[u8_i]) & 0x01);
+#elif Int_Priority == XYY
+        u8_IRQ_Priority = (((S_UserConfig->Group_Priority[u8_i]) & 0x01) << 2UL) | ((S_UserConfig->Sub_Group_Priority[u8_i]) & 0x03);
+#elif Int_Priority == YYY
+        u8_IRQ_Priority = ((S_UserConfig->Sub_Group_Priority[u8_i]) & 0x07);
+#endif
+        IntCtrl_ConfigPriority(u8_i, u8_IRQ_Priority);
+        /*Enable/Disable based on user configuration in NVIC_ENx and SCB_Sys Registers*/
+        if(S_UserConfig->Enabled[u8_i]){
+            IntCtrl_EnableIRQ(u8_i);
+        }
+    }
+}
 
 /***************************************************************************************
  *                               End of File: IntCtrl.c                                *
-****************************************************************************************/
+ ****************************************************************************************/
